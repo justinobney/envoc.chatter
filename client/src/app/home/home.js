4c454ef9217a6fb987ed54d6fc63023d81041c16
Justin Obney <justinobney@gmail.com>
2014-11-20 12:00:11 -0600

    feat(presence): adding simple presence system

--- client/src/app/home/home.js
+++ client/src/app/home/home.js
@@ -1,45 +1,57 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  /**
-   * @name  config
-   * @description config block
-   */
-  function config($stateProvider) {
-    $stateProvider
-      .state('root.home', {
-        abstract: true,
-        resolve: {
-          currentUser: function(auth) {
-            return auth.$requireAuth();
-          }
-        },
-        views: {
-          '@': {
-            templateUrl: 'src/app/home/home.tpl.html',
-            controller: 'HomeCtrl as home'
-          }
-        }
-      });
-  }
-
-  /**
-   * @name  HomeCtrl
-   * @description Controller
-   */
-  function HomeCtrl(channelService, fbutil) {
-    var home = this;
-
-    home.channels = fbutil.syncObject('channels');
-    home.addChannel = addChannel;
-
-    function addChannel(){
-      channelService.addChannel(home.channelName);
-      home.channelName = '';
+    /**
+     * @name  config
+     * @description config block
+     */
+    function config($stateProvider) {
+        $stateProvider
+            .state('root.home', {
+                abstract: true,
+                resolve: {
+                    currentUser: function(auth) {
+                        return auth.$requireAuth();
+                    }
+                },
+                views: {
+                    '@': {
+                        templateUrl: 'src/app/home/home.tpl.html',
+                        controller: 'HomeCtrl as home'
+                    }
+                }
+            });
     }
-  }
 
-  angular.module('home', [])
-    .config(config)
-    .controller('HomeCtrl', HomeCtrl);
+    /**
+     * @name  HomeCtrl
+     * @description Controller
+     */
+    function HomeCtrl(channelService, fbutil, session) {
+        var home = this;
+        var connected = fbutil.ref('/.info/connected');
+
+        home.channels = fbutil.syncObject('channels');
+        home.people = fbutil.syncObject('profiles');
+        home.addChannel = addChannel;
+        session.user.$loaded().then(bindPresence);
+
+        function addChannel() {
+            channelService.addChannel(home.channelName);
+            home.channelName = '';
+        }
+
+        function bindPresence(){
+          var presence = fbutil.ref(['profiles', session.uid, 'online'])
+          connected.on("value", function(isOnline) {
+            presence.set(isOnline.val());
+          });
+
+          presence.onDisconnect().remove();
+        }
+    }
+
+    angular.module('home', [])
+        .config(config)
+        .controller('HomeCtrl', HomeCtrl);
 })();
