e2533f103a8c650bcde2a9f1999fdec82386cf66
Justin Obney <justinobney@gmail.com>
2014-11-20 23:37:27 -0600

    feat(channels): only show joined channels

--- client/src/common/filters/uppercase.js
+++ client/src/common/filters/uppercase.js
@@ -10,3 +10,33 @@
   angular.module('common.filters.uppercase', [])
     .filter('uppercase', uppercase);
 })();
+
+(function() {
+  'use strict';
+
+  function activeChannel(session, fbutil) {
+    return function(input) {
+      return _.where(input, function(value, key){
+        return key[0] !== '$' && session.channels[key];
+      });
+    };
+  }
+
+  angular.module('common.filters.uppercase')
+    .filter('activeChannel', activeChannel);
+})();
+
+(function() {
+  'use strict';
+
+  function inactiveChannelCount(session, fbutil) {
+    return function(input) {
+      return _.where(input, function(value, key){
+        return key[0] !== '$' && !session.channels[key];
+      }).length;
+    };
+  }
+
+  angular.module('common.filters.uppercase')
+    .filter('inactiveChannelCount', inactiveChannelCount);
+})();
